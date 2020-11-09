c66caf59163194c1ff3f3788c2444729a7c1c1de
fehty <alioleg8@gmail.com>
2020-11-09 22:39:58 +0300

    Latest changes

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ from sprite_combinating import create_sprites_and_json
 from sprite_cropping import save_cropped_objs
 from sprite_stylizing import create_stylized_imgs
 
+# Objects
 # init_objs = '/home/fehty/BlenderCompilation/BlenderRes/EachFrameRender/'
-init_objs = '/home/fehty/BlenderCompilation/BlenderRes/ClosestRotateRender/objectBookshelf_002/'
+# init_objs = '/home/fehty/BlenderCompilation/BlenderRes/ClosestRotateRender/objectBookshelf_002/'
 cropped_objs = '/home/fehty/PycharmProjects/SpriteLauncher/CroppedObjects/'
 stylized_cropped_objs = '/home/fehty/PycharmProjects/SpriteLauncher/StylizedObjects/'
 saved_sprites = '/home/fehty/PycharmProjects/SpriteLauncher/Sprites/'
 
+# Images
+init_objs = '/home/fehty/BlenderCompilation/BlenderRes/ExportedImages/'
 
 if __name__ == '__main__':
-    # save_cropped_objs()
-    # create_stylized_imgs()
-    # create_sprites_and_json()
-    common_foldering(stylized_cropped_objs, '/home/fehty/PycharmProjects/SpriteLauncher/CommonObjects/')
+    # Objects
+    save_cropped_objs()
+    create_stylized_imgs()
+    create_sprites_and_json()
+
+    # Images
+
+    # common_foldering("/home/fehty/PycharmProjects/SpriteLauncher/CroppedObjects3/", '/home/fehty/PycharmProjects/SpriteLauncher/CommonObjects/')
