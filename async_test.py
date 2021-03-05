5ba738f7f8f570a7c26245e993cb654d8b6ade68
fehty <alioleg8@gmail.com>
2021-03-05 19:16:26 +0300

    First version of async crop for animation

--- async_test.py
+++ async_test.py
@@ -1,25 +1,44 @@
 import asyncio
 
+# async def factorial(name, number):
+#     f = 1
+#     for i in range(2, number + 1):
+#         print(f"Task {name}: Compute factorial({i})...")
+#         await asyncio.sleep(1)
+#         f *= i
+#     print(f"Task {name}: factorial({number}) = {f}")
+#
+#
+# async def main():
+#     # Schedule three calls *concurrently*:
+#     await asyncio.gather(
+#         factorial("A", 2),
+#         factorial("B", 3),
+#         factorial("C", 4),
+#     )
+#
+#
+# loop = asyncio.get_event_loop()
+#
+# result = loop.run_until_complete(main())
+
+import asyncio
+
 
 async def first():
-    await asyncio.sleep(1)
-    return "1"
+    for i in range(0, 10):
+        await asyncio.sleep(0.1)
+        print(i)
 
 
 async def second():
-    await asyncio.sleep(1)
-    return "2"
+    for i in range(10, 20):
+        await asyncio.sleep(0.1)
+        print(i)
 
 
 async def main():
-    async def one_iteration():
-        result = await first()
-        print(result)
-        result2 = await second()
-        print(result2)
-
-    # coros = [one_iteration() for _ in range(2)]
-    await asyncio.gather(one_iteration(), one_iteration())
+    await asyncio.gather(first(), second())
 
 
 loop = asyncio.get_event_loop()
--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from sprite_combinating import create_sprites_and_json
 from sprite_cropping_for_animation import save_cropped_objs
 from sprite_stylizing import create_stylized_imgs
 from resolution_division import division_launcher
+import asyncio
 
+import sys
 # Objects
 # init_objs = '/home/fehty/BlenderCompilation/BlenderRes/EachFrameRender/'
 # init_objs = '/home/fehty/BlenderCompilation/BlenderRes/ClosestRotateRender/'
 from webp_creation import webp_creation
 
-init_objs = '/home/fehty/BlenderCompilation/BlenderRes/RendersRotateAround/object24_bod_1_0_0_001_another_copy/'
-cropped_objs = '/home/fehty/BlenderCompilation/BlenderRes/RendersRotateAround/object24_bod_1_0_0_001_another_copy/cropped/'
+init_objs = '/home/fehty/BlenderCompilation/BlenderRes/RendersRotateAround/object24_bod_1_0_0_001_copy_async/'
+cropped_objs = '/home/fehty/BlenderCompilation/BlenderRes/RendersRotateAround/object24_bod_1_0_0_001_copy_async/cropped/'
 # cropped_objs = '/home/fehty/PycharmProjects/SpriteLauncher/CroppedObjects/'
 # stylized_cropped_objs = '/home/fehty/PycharmProjects/SpriteLauncher/StylizedCroppedObjects/'
 # stylized_cropped_objs = '/home/fehty/BlenderCompilation/BlenderRes/RendersRotateAround/objectBody001/test_final/cropped/'
@@ -27,7 +29,16 @@ cropped_objs = '/home/fehty/BlenderCompilation/BlenderRes/RendersRotateAround/ob
 
 if __name__ == '__main__':
     # Objects
+    # async def run_cropping_async():
+    #     await
+    # asyncio.run(save_cropped_objs())
+    import time
+    s = time.perf_counter()
     save_cropped_objs()
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+
+
     # create_stylized_imgs()
     # division_launcher()
     # create_sprites_and_json()
